5fdd2c041eace36cd04e8b2be43ba55eda5b263c
murodjon <murodjonisroiljonov4@gmail.com>
2024-11-14 04:42:00 +0500

    tailwind test

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "text-gray": "#6B7280",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
