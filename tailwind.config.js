f3204c570f15b682ae271e22a96b82337c512d6a
murodjon <murodjonisroiljonov4@gmail.com>
2024-11-14 05:53:54 +0500

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,17 @@ module.exports = {
     extend: {
       colors: {
         "text-gray": "#6B7280",
+        "text-blue": "#4F46E5",
       },
+
       fontFamily: {
         inter: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        "custom-sm": "0px 1px 2px 0px #0000000F", // Maxsus soyani qo'shish
+      },
+      boxShadow: {
+        "custom-md": "0px 1px 3px 0px #0000001A", // Maxsus soyani qo'shish
       },
     },
   },
